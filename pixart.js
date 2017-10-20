12d28624d52b43852c879b3f873b7dbca588a22a
arielse <ariel@bottledup.net>
2017-10-20 14:55:42 +1100

    used append to add in 1000 divs with square class to body of page

--- pixart.js
+++ pixart.js
@@ -1,9 +1,16 @@
 var $brush = $(".brush");
 var $button = $("#set-color");
 var $colorField = $("#color-field");
+var body = $("body");
 
 $button.on('click', function(event) {
   event.preventDefault();
   $brush.css({"background-color":$colorField.val()})
   $colorField.val('');
-})
+});
+
+
+for (var i = 0; i <= 1000; i++) {
+  var $square = $("<div>",{ "class": "square"});
+  $(body).append($square);
+}
