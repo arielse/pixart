3ea0c1b50da907e799799c591b30400227bba2a6
arielse <ariel@bottledup.net>
2017-10-20 15:05:42 +1100

    commit 4, can click on each square class to make it green

--- pixart.js
+++ pixart.js
@@ -1,16 +1,22 @@
 var $brush = $(".brush");
 var $button = $("#set-color");
 var $colorField = $("#color-field");
-var body = $("body");
+var $body = $("body");
+var $squareDiv = $(".square");
+
 
 $button.on('click', function(event) {
   event.preventDefault();
-  $brush.css({"background-color":$colorField.val()})
+  $brush.css({"background-color":$colorField.val()});
   $colorField.val('');
 });
 
 
 for (var i = 0; i <= 1000; i++) {
   var $square = $("<div>",{ "class": "square"});
-  $(body).append($square);
-}
+  $($body).append($square);
+};
+
+$body.on('click', ".square", function(event) {
+  $(event.target).css({"background":"green"});
+})
