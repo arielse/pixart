a44f50266751e3c99f4228ffdc03dbeb8df5d174
arielse <ariel@bottledup.net>
2017-10-20 14:36:57 +1100

    commit 1, set box colour to value of input field on button click, prevent page from reloading

---
+++ pixart.js
@@ -0,0 +1,8 @@
+var $brush = $(".brush");
+var $button = $("#set-color");
+var $colorField = $("#color-field");
+
+$button.on('click', function(event) {
+  event.preventDefault();
+  $brush.css({"background-color":$colorField.val()})
+})
